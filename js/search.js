a26acc9297bc1ed25e1a9255b98a45c3a60db662
Eric Galvan <35750901+Gooosifer@users.noreply.github.com>
2023-07-17 12:58:23 -0700

    functional search, more UI improvements

--- js/search.js
+++ js/search.js
@@ -1,55 +1,61 @@
-'use strict'
+'use strict';
+
 window.SearchApp = {
-    searchField: document.getElementById("searchField"),
-    searchButton: document.getElementById("searchButton"),
-    output: document.getElementById("output"),
-    searchData: {},
-    searchIndex: {}
+  output: document.getElementById("output"),
+  searchData: {},
+  searchIndex: {},
 };
 
-axios
-.get('/search/index.json')
-    .then(response => {
-        SearchApp.searchData = response.data;
-        SearchApp.searchIndex = lunr(function () {
-        this.ref('href');
-        this.field('title');
-        this.field('body');
-        response.data.results.forEach(e => {
-                this.add(e);
-            });
-    });
-});
+// Retrieve the query value from the URL parameter
+const urlParams = new URLSearchParams(window.location.search);
+const query = urlParams.get('query');
 
-SearchApp.searchButton.addEventListener('click', search);
-function search() {
-    let searchText = SearchApp.searchField.value;
-    let resultList = SearchApp.searchIndex.search(searchText);
+axios.get('/search/index.json', {
+  params: {
+    query: query,
+  },
+})
+  .then(result => {
+    SearchApp.searchData = result.data;
+    SearchApp.searchIndex = lunr(function () {
+      this.ref('href');
+      this.field('title');
+      this.field('body');
+      result.data.results.forEach(e => {
+        this.add(e);
+      });
+    });
+
+    let resultList = SearchApp.searchIndex.search(query);
     let list = [];
     let results = resultList.map(entry => {
-        SearchApp.searchData.results.filter(d => {
-            if(entry.ref == d.href) {
-                list.push(d);
-            }
-        })
+      SearchApp.searchData.results.filter(d => {
+        if (entry.ref == d.href) {
+          list.push(d);
+        }
+      });
     });
     display(list);
-}
+  });
 
 function display(list) {
-    SearchApp.output.innerText = '';
-    if (list.length > 0) {
-        const ul = document.createElement("ul");
-        list.forEach(el => {
-            const li = document.createElement("li");
-            const a = document.createElement("a");
-            a.href = el.href;
-            a.text = el.title;
-            li.appendChild(a);
-            ul.appendChild(li);
-        });
-        SearchApp.output.appendChild(ul);
-    } else {
-        SearchApp.output.innerHTML = "Nothing found";
-    }
-};
+  SearchApp.output.innerText = '';
+  if (list.length > 0) {
+    const ul = document.createElement("ul");
+    ul.classList.add("mod-nav-items");
+    list.forEach(el => {
+      const li = document.createElement("li");
+      li.classList.add("mod-nav-item");
+      const a = document.createElement("a");
+      a.href = el.href;
+      a.text = el.title;
+      li.appendChild(a);
+      ul.appendChild(li);
+      
+    });
+    
+    SearchApp.output.appendChild(ul);
+  } else {
+    SearchApp.output.innerHTML = "Nothing found";
+  }
+}
